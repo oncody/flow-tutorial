36031cd086492e5ba4ea2e189dce88416ad52a73
Cody Smith <37386486+oncody@users.noreply.github.com>
2020-01-20 03:09:22 -0700

    removing unused code

--- 03-babel-eslint/.eslintrc.js
+++ 03-babel-eslint/.eslintrc.js
@@ -2,24 +2,3 @@ module.exports = {
   extends: ["plugin:flowtype-errors/recommended"],
   parser: 'babel-eslint'
 };
-
-// module.exports = {
-  // plugins: [
-  //   'flowtype-errors'
-  // ],
-  // rules: {
-  //   'flowtype-errors/show-errors': 'error',
-  //   'flowtype-errors/show-warnings': 'warn'
-  // }
-//   plugins: [
-//     'flowtype'
-//   ],
-//   extends: [
-//     'plugin:flowtype/recommended'
-//   ],
-//   'settings': {
-//     'flowtype': {
-//       'onlyFilesWithFlowAnnotation': false
-//     }
-//   }
-// };
